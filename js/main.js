6271849e3444ee6f9be16812c8c8b784a3025d9e
WXDAndrew <andres.w.carrillo1@gmail.com>
2024-12-17 22:31:21 -0500

    aos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Inicializa AOS
+AOS.init({
+    offset: 120, // Ajusta cuándo se activa la animación
+    duration: 600, // Duración en milisegundos
+    easing: 'ease-in-out', // Tipo de animación
+    once: true, // Ejecutar animación solo una vez
+});
